4ed9719e35aaf5438e69c7ce178e3675510ee337
Ivan Ubuntu <arigatory@gmail.com>
2021-01-17 09:03:07 -0800

    add eq2

--- main.pas
+++ main.pas
@@ -19,17 +19,36 @@ begin
 	writeln('cos(',x:0:0,') = ',cos(x):0:3);
 end;
 
+
+procedure eq2();
 var
-	s:string;
+	a,b,c,d,x1,x2:real;
+begin
+	write('Please enter a: ');
+	readln(a);
+	write('Please enter b: ');
+	readln(b);
+	write('Please enter c: ');
+	readln(c);
+	writeln('Solving: ', a:0:0, 'x^2 + ', b:0:0, 'x + ', c:0:0, ' = 0');
+end;
+
+
+
+
+
+var
+	command:string;
 	next:boolean;
 begin
 	next := true;
 	while next do begin
 		write('> ');
-		readln(s);
-		if s = 'sin' then sinus()
-		else if s = 'exit' then next := false
-		else if s = 'cos' then cosinus();
+		readln(command);
+		if command = 'sin' then sinus()
+		else if command = 'cos' then cosinus()
+		else if command = 'eq2' then eq2()
+		else if command = 'exit' then next := false;
 		{add precedures 1) ax^2+bx+c = 0 2) no such command }
 	end;
 end.
